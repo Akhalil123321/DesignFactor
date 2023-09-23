73cde8b8791fcff597c4ca8168dc2e6a950107cf
Ahmed khalil <ahmed.mohamed.khalil1998@gmail.com>
2023-09-23 21:41:04 +0400

    .

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import { useState } from 'react'
 import "./NavBar.css"
-
+import part1 from "../../Images/Logo/part1.png"
+import part2 from "../../Images/Logo/part2.png"
+import part3 from "../../Images/Logo/part3.png"
+import part4 from "../../Images/Logo/part4.png"
+import part5 from "../../Images/Logo/part5.png"
 const NavBar = () => {
   const [list, setList] = useState(false)
   const listState = list ? 'fa-icon-open':'fa-icon-close'
   const circleState = list ? 'circle-open' : 'circle-close'
+  const fa1State = list ? 'fa1-open' : 'fa1-close'
+  const fa2State = list ? 'fa2-open' : 'fa2-close'
+  const fa3State = list ? 'fa3-open' : 'fa3-close'
+
   return (
     <div className='nav'>
     <div className={listState} onClick={() => setList(!list)}>
-      <span className='fa-line1'></span>
-      <span className='fa-line2'></span>
-      <span className='fa-line3'></span>
+      <span className={fa1State}></span>
+      <span className={fa2State}></span>
+      <span className={fa3State}></span>
     </div>
-      <img src='' alt=''/>
+    <div className='Nav-Logo'>
+        <img src={part1} alt='' className='nav-logo-parts nav-logo-part1'/>
+        <img src={part2} alt='' className='nav-logo-parts nav-logo-part2'/>
+        <img src={part3} alt='' className='nav-logo-parts nav-logo-part3'/>
+        <img src={part4} alt='' className='nav-logo-parts nav-logo-part4'/>
+        <div className='part5-parent'><img src={part5} alt='' className='nav-logo-parts nav-logo-part5'/></div>
+    </div>
       <div className={circleState}></div>
     </div>
   )
