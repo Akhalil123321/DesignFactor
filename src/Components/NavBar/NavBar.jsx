310a6286f4e26edaa6a5c41bdd6927549d919f2d
Ahmed khalil <ahmed.mohamed.khalil1998@gmail.com>
2023-09-25 23:45:10 +0400

    ..

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,10 +3,9 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import "./NavBar.css"
 import part1 from "../../Images/Logo/part1.png"
-import part2 from "../../Images/Logo/part2.png"
-import part3 from "../../Images/Logo/part3.png"
-import part4 from "../../Images/Logo/part4.png"
-import part5 from "../../Images/Logo/part5.png"
+import part2 from "../../Images/Logo/part6.png"
+import part3 from "../../Images/Logo/part5.png"
+// import chatIcon from "../../Images/chat.svg"
 const NavBar = () => {
   const [list, setList] = useState(false);
   const [scrolled, setScrolled] = useState(false);
@@ -31,6 +30,9 @@ const NavBar = () => {
     };
   }, []);
   const scrollState = scrolled ? 'nav-scroll-down' : 'nav-scroll-up'
+  const logoState1 = scrolled ? 'nav-logo-parts nav-logo-part1-close' : 'nav-logo-parts nav-logo-part1'
+  const logoState2 = scrolled ? 'nav-logo-part2-close' : 'nav-logo-part2'
+  const logoState3 = scrolled ? 'nav-logo-parts nav-logo-part3-close' : 'nav-logo-parts nav-logo-part3'
 
 
   return (
@@ -41,13 +43,16 @@ const NavBar = () => {
       <span className={fa3State}></span>
     </div>
     <div className='Nav-Logo'>
-        <img src={part1} alt='' className='nav-logo-parts nav-logo-part1'/>
-        <img src={part2} alt='' className='nav-logo-parts nav-logo-part2'/>
-        <img src={part3} alt='' className='nav-logo-parts nav-logo-part3'/>
-        <img src={part4} alt='' className='nav-logo-parts nav-logo-part4'/>
-        <div className='part5-parent'><img src={part5} alt='' className='nav-logo-parts nav-logo-part5'/></div>
+        <img src={part1} alt='' className={logoState1}/>
+        <div className='part2-parent'><img src={part2} alt='' className={logoState2}/></div>
+        <div className='part3-parent'><img src={part3} alt='' className={logoState3}/></div>
     </div>
       <div className={circleState}></div>
+        {/* <img src={chatIcon} alt='' className='chat-icon'/> */}
+        <svg xmlns="http://www.w3.org/2000/svg" width="128" height="40" viewBox="0 0 128 40" >
+          <path d="M16.216,0h94.8a16.216,16.216,0,1,1,0,32.432v7.7l-13.646-7.7H16.216A16.216,16.216,0,0,1,16.216,0Z" ></path>
+        </svg>
+      {/* <div className='Contact-us'>Contact Us</div> */}
     </div>
   )
 }
