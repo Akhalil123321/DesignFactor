520b9142a3f67bdbf9c918bc030bac4f96519d8b
Ahmed khalil <ahmed.mohamed.khalil1998@gmail.com>
2023-09-26 23:20:51 +0400

    .

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { FaAngleDown } from "react-icons/fa";
 import "./NavBar.css"
 import part1 from "../../Images/Logo/part1.png"
 import part2 from "../../Images/Logo/part6.png"
 import part3 from "../../Images/Logo/part5.png"
-// import chatIcon from "../../Images/chat.svg"
 const NavBar = () => {
   const [list, setList] = useState(false);
   const [scrolled, setScrolled] = useState(false);
@@ -37,10 +37,12 @@ const NavBar = () => {
 
   return (
     <div className={scrollState}>
-    <div className={listState} onClick={() => setList(!list)}>
-      <span className={fa1State}></span>
-      <span className={fa2State}></span>
-      <span className={fa3State}></span>
+    <div className='fa-container'>
+      <div className={listState} onClick={() => setList(!list)}>
+        <span className={fa1State}></span>
+        <span className={fa2State}></span>
+        <span className={fa3State}></span>
+      </div>
     </div>
     <div className='Nav-Logo'>
         <img src={part1} alt='' className={logoState1}/>
@@ -48,11 +50,22 @@ const NavBar = () => {
         <div className='part3-parent'><img src={part3} alt='' className={logoState3}/></div>
     </div>
       <div className={circleState}></div>
-        {/* <img src={chatIcon} alt='' className='chat-icon'/> */}
-        <svg xmlns="http://www.w3.org/2000/svg" width="128" height="40" viewBox="0 0 128 40" >
-          <path d="M16.216,0h94.8a16.216,16.216,0,1,1,0,32.432v7.7l-13.646-7.7H16.216A16.216,16.216,0,0,1,16.216,0Z" ></path>
-        </svg>
-      {/* <div className='Contact-us'>Contact Us</div> */}
+      {/* <div className='Nav-right'>
+          <span className='languages'>Language</span>
+          <FaAngleDown className='arrow-icon'/>
+      </div> */}
+      <div className='right-container'>
+        <div className='right-container-ch1'>
+        <span className='right-container-ch1-title'>Draft</span>
+          <div className='right-container-ch11'></div>
+          <div className='right-container-ch12'></div>
+        </div>
+        <div className='right-container-ch2'>
+        <span className='right-container-ch2-title'>Draft</span>
+          <div className='right-container-ch21'></div>
+          <div className='right-container-ch22'></div>
+        </div>
+      </div>
     </div>
   )
 }
