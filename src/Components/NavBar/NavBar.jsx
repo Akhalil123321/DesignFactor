1ecf7c42302c051ab9086f61c63f1d79bc3246bc
Ahmed khalil <ahmed.mohamed.khalil1998@gmail.com>
2023-09-29 21:24:21 +0400

    .

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { MdLanguage } from "react-icons/md";
 import { BiMessageRoundedDetail } from "react-icons/bi";
 import { FaCouch } from "react-icons/fa";
+import { FaBorderAll } from "react-icons/fa6";
+import { RiFridgeFill } from "react-icons/ri";
+import { GiElectricalSocket } from "react-icons/gi";
+import { CgGym } from "react-icons/cg";
+import { BsDoorOpenFill } from "react-icons/bs";
+import { FaToilet } from "react-icons/fa";
+import { GiTheaterCurtains } from "react-icons/gi";
+import { GiMarbles } from "react-icons/gi";
+import { BsBuildings } from "react-icons/bs";
 import "./NavBar.css"
 import part1 from "../../Images/Logo/part1.png";
 import part2 from "../../Images/Logo/part6.png";
 import part3 from "../../Images/Logo/part5.png";
+
 const NavBar = () => {
   const [list, setList] = useState(false);
+  const [categories, setCategories] = useState(false);
   const [scrolled, setScrolled] = useState(false);
 
   const listState = list ? 'fa-icon-open':'fa-icon-close'
@@ -17,6 +27,14 @@ const NavBar = () => {
   const fa1State = list ? 'fa1-open' : 'fa1-close'
   const fa2State = list ? 'fa2-open' : 'fa2-close'
   const fa3State = list ? 'fa3-open' : 'fa3-close'
+  const categoriesClose1 = categories ? 'categories-close-icon-x-1-o' : 'categories-close-icon-x-1'
+  const categoriesClose2 = categories ? 'categories-close-icon-x-2-o' : 'categories-close-icon-x-2'
+  const categoriesText = categories? 'categories-icon-text-o' : 'categories-icon-text'
+  const categoriesClose = categories? 'categories-icon-close-o' : 'categories-icon-close'
+  const categoriesCircle = categories? 'categories-icon-circle-o' : 'categories-icon-circle'
+  const categoriesAll = categories? 'categories-all-o' : 'categories-all'
+  const catCircleAll = categories? 'cat-circle-all-o' : 'cat-circle-all'
+  const blurFilter = categories? 'blur-filter' : 'blur-filter-o'
 
   useEffect(() => {
     const handleScroll = () => {
@@ -35,26 +53,40 @@ const NavBar = () => {
   const logoState1 = scrolled ? 'nav-logo-parts nav-logo-part1-close' : 'nav-logo-parts nav-logo-part1'
   const logoState2 = scrolled ? 'nav-logo-part2-close' : 'nav-logo-part2'
   const logoState3 = scrolled ? 'nav-logo-parts nav-logo-part3-close' : 'nav-logo-parts nav-logo-part3'
-  /*const contactScroll = scrolled ? 'right-container-ch1-down' : 'right-container-ch1'*/
-  /*const contactIcon = scrolled ? 'contact-icon-down' : 'contact-icon'*/
-  /* const projectsScroll = scrolled ? 'right-container-ch2-down' : 'right-container-ch2'*/
-  /* const projectsIcon = scrolled ? 'projects-icon-down' : 'projects-icon'*/
-
 
   return (
     <div className={scrollState}>
     <div className='fa-container'>
-      <div className={listState} onClick={() => setList(!list)}>
+      <div className={listState} onClick={() => {setList(!list); setCategories(false)}}>
         <span className={fa1State}></span>
         <span className={fa2State}></span>
         <span className={fa3State}></span>
       </div>
-      {/*<div className={projectsScroll}>
-        <span className='right-container-ch2-title'>Projects</span>
-          <div className='right-container-ch21'></div>
-          <div className='right-container-ch22'></div>
-        </div>*/}
-        <FaCouch className= 'projects-icon-down'/>
+        <div className='categories-icon-cont'>
+        <div className={blurFilter} onClick={() =>setCategories(false)}></div>
+        <div className={categoriesCircle}>
+          <div className={`furniture-box ${catCircleAll}`} onClick={() => setCategories(false)}><FaCouch className='cat-icon'/><span className='cat-text'>Furniture</span></div>
+          <div className={`Appliances-box ${catCircleAll}`} onClick={() => setCategories(false)}><RiFridgeFill className='cat-icon'/><span className='cat-text'>Appliances</span></div>
+          <div className={`Ceramics-box ${catCircleAll}`} onClick={() => setCategories(false)}><FaBorderAll className='cat-icon'/><span className='cat-text'>Ceramics</span></div>
+          <div className={`Sanitary-box ${catCircleAll}`} onClick={() => setCategories(false)}><FaToilet className='cat-icon'/><span className='cat-text'>Sanitary</span></div>
+          <div className={`Sockets-box ${catCircleAll}`} onClick={() => setCategories(false)}><GiElectricalSocket className='cat-icon'/><span className='cat-text'>Sockets</span></div>
+          <div className={`GYM-box ${catCircleAll}`} onClick={() => setCategories(false)}><CgGym className='cat-icon'/><span className='cat-text'>GYM</span></div>
+          <div className={`Doors-box ${catCircleAll}`} onClick={() => setCategories(false)}><BsDoorOpenFill className='cat-icon'/><span className='cat-text'>Doors</span></div>
+          <div className={`Fit-out-box ${catCircleAll}`} onClick={() => setCategories(false)}><BsBuildings className='cat-icon'/><span className='cat-text'>Fit-out</span></div>
+          <div className={`Marble-box ${catCircleAll}`} onClick={() => setCategories(false)}><GiMarbles className='cat-icon'/><span className='cat-text'>Marble</span></div>
+          <div className={`Curtains-box ${catCircleAll}`} onClick={() => setCategories(false)}><GiTheaterCurtains className='cat-icon'/><span className='cat-text'>Curtains</span></div>
+        </div>
+          <div className= 'categories-icon' onClick={() => setCategories(!categories)}>
+            <div className={categoriesClose1}></div>
+            <div className={categoriesClose2}></div>
+            <div alt='Furniture' className={`categories-icon-1 ${categoriesAll}`}></div>
+            <div alt='Appliances' className={`categories-icon-2 ${categoriesAll}`}></div>
+            <div alt='Ceramics' className={`categories-icon-3 ${categoriesAll}`}></div>
+            <div alt='Sanitary' className={`categories-icon-4 ${categoriesAll}`}></div>
+        </div>
+          <span className={categoriesText}>Categories</span>
+          <span className={categoriesClose}>Close</span>
+        </div>
     </div>
     <div className='Nav-Logo'>
         <img src={part1} alt='' className={logoState1}/>
@@ -63,13 +95,11 @@ const NavBar = () => {
     </div>
       <div className={circleState}></div>
       <div className='right-container'>
-        <BiMessageRoundedDetail className= 'contact-icon-down'/>
-        {/*<div className={contactScroll}>
-          <span className='right-container-ch1-title'>Contact </span>
-          <div className='right-container-ch11'></div>
-          <div className='right-container-ch12'></div>
-        </div>*/}
-        <MdLanguage className='languages-icon'/>
+        <div className= 'contact-icon-cont' onClick={() =>setCategories(false)}>
+          <BiMessageRoundedDetail className= 'contact-icon'/>
+          <span className='contact-icon-text'> Contact us</span>
+        </div>
+        <MdLanguage className='languages-icon' onClick={() =>setCategories(false)}/>
       </div>
     </div>
   )
