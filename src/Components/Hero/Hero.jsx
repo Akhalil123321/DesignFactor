6a0088ce7ffc919cc440015bfc62cfde6a50a707
Ahmed khalil <ahmed.mohamed.khalil1998@gmail.com>
2023-11-10 19:55:43 +0400

    .

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,31 +9,60 @@ import point from "../../Images/Logo/part5.png"
 
 const Hero = () => {
   const [countOn, setCountOn] = useState(false)
-  const [tiltX, setTiltX] = useState(0);
-  const [tiltY, setTiltY] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragStartX, setDragStartX] = useState(0);
+  const imagesToMove = 2;
 
-  const handleMouseMove = (e) => {
-    const circle = document.getElementById('circle-360');
-    const boundingBox = circle.getBoundingClientRect();
-    const centerX = boundingBox.left + boundingBox.width / 2;
-    const centerY = boundingBox.top + boundingBox.height / 2;
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-    const relativeX = mouseX - centerX;
-    const relativeY = mouseY - centerY;
-    const maxTilt = 3;
-    const tiltX = (relativeY / centerY) * maxTilt * 20;
-    const tiltY = (-relativeX / centerX) * maxTilt * 20;
-    setTiltX(tiltX);
-    setTiltY(tiltY);
-  };
-  const handleMouseLeave = () => {
-    setTiltX(0);
-    setTiltY(0);
-  };
-  function open360(){
+  const open360N1 = () => {
     window.open('https://kuula.co/share/collection/7YCCl?logo=1&info=0&logosize=122&fs=1&vr=1&gyro=0&autorotate=0.12&thumbs=-1&inst=0&gcl=1','_black').focus()
 }
+  const open360N2 = () => {
+    window.open('https://kuula.co/share/collection/7PDsp?fs=1&vr=1&zoom=1&gyro=0&autorotate=0.1&thumbs=-1&inst=0&info=0&logo=1&logosize=120&gcl=1','_black').focus()
+}
+  const open360N3 = () => {
+    window.open('https://kuula.co/share/collection/7kCDD?logo=1&info=0&logosize=120&fs=1&vr=1&gyro=0&autorotate=0.04&thumbs=-1&inst=0&keys=0','_black').focus()
+}
+  const open360N4 = () => {
+    window.open('https://kuula.co/share/collection/79KtX?logo=1&info=0&logosize=152&fs=1&vr=1&gyro=0&thumbs=1','_black').focus()
+}
+  const open360N5 = () => {
+    window.open('https://kuula.co/share/collection/7vj9b?logo=1&info=0&logosize=120&fs=1&vr=0&gyro=0&autorotate=-0.03&thumbs=-1&inst=0&keys=0','_black').focus()
+}
+const handleMouseDown = (e) => {
+  setIsDragging(true);
+  setDragStartX(e.clientX);
+};
+
+const handleMouseMove = (e) => {
+  if (isDragging) {
+    const draggedDistance = e.clientX - dragStartX;
+    const movementThreshold = 50 * imagesToMove;
+
+    if (draggedDistance > movementThreshold) {
+      moveImagesToLeft(imagesToMove);
+      setIsDragging(false);
+    } else if (draggedDistance < -movementThreshold) {
+      moveImagesToRight(imagesToMove);
+      setIsDragging(false);
+    }
+  }
+};
+const moveImagesToRight = (count) => {
+  let lists = document.querySelectorAll('.img-360');
+  for (let i = 0; i < count; i++) {
+    document.getElementById('slide').appendChild(lists[i]);
+  }
+};
+const moveImagesToLeft = (count) => {
+  let lists = document.querySelectorAll('.img-360');
+  for (let i = lists.length - 1; i >= lists.length - count; i--) {
+    document.getElementById('slide').prepend(lists[i]);
+  }
+};
+const handleMouseUp = () => {
+  setIsDragging(false);
+};
+
   return (
     <div className='hero'>
       <ScrollTrigger onEnter={() => setCountOn(true)}>
@@ -66,28 +95,61 @@ const Hero = () => {
 			</div>
 			</ScrollTrigger>
       <div className='hero-about-us-container'>
-        <div className='hero-about-us-content1'>
-          <img src={largeImg} className='large-img-360' alt='' onClick={open360}/>
-          <div
-            onClick={open360}
-            id="circle-360"
-            className="hero-circle-360"
-            onMouseMove={handleMouseMove}
-            onMouseLeave={handleMouseLeave}
-            style={{
-            transform: `perspective(1000px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`,
-            }}>
-            <div className='circle-inside-text'><span>Discover Our Virtual Tour</span></div>
-              <img src={smallImg} className='small-img-360' alt=''/>
-              <div className='small-img-360-shadow'></div>
-              <div className='hor-circle-360'>
-                <div className='hor-circle-360-ball'></div>
-              </div>
-              <div className='ver-circle-360'>
-                <div className='ver-circle-360-ball'></div>
-              </div>
+      <div className="hero-about-us-content1">
+        <div 
+        id="slide"
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        >
+            <div className="img-360 img-360-1" onMouseDown={handleMouseDown}>
+                <div className="content">
+                    <div className="name">Project Name</div>
+                    <div className="des">Step into a world of wonder with our captivating virtual tour! Explore every corner and detail for our works.</div>
+                    <button className='img-360-button' onClick={open360N1}>Virtual Tour</button>
+                </div>
             </div>
+            <div className="img-360 img-360-2 img-small-style" onMouseDown={handleMouseDown} onClick={open360N1}></div>
+            <div className="img-360 img-360-3" onMouseDown={handleMouseDown}>
+                <div className="content">
+                    <div className="name">Project Name</div>
+                    <div className="des">Step into a world of wonder with our captivating virtual tour! Explore every corner and detail for our works.</div>
+                    <button className='img-360-button' onClick={open360N2}>Virtual Tour</button>
+                </div>
+            </div>
+            <div className="img-360 img-360-4 img-small-style" onMouseDown={handleMouseDown} onClick={open360N2}></div>
+            <div className="img-360 img-360-5" onMouseDown={handleMouseDown}>
+                <div className="content">
+                    <div className="name">Project Name</div>
+                    <div className="des">Step into a world of wonder with our captivating virtual tour! Explore every corner and detail for our works.</div>
+                    <button className='img-360-button' onClick={open360N3}>Virtual Tour</button>
+                </div>
+            </div>
+            <div className="img-360 img-360-6 img-small-style" onMouseDown={handleMouseDown} onClick={open360N3}></div>
+            <div className="img-360 img-360-7" onMouseDown={handleMouseDown}>
+                <div className="content">
+                    <div className="name">Project Name</div>
+                    <div className="des">Step into a world of wonder with our captivating virtual tour! Explore every corner and detail for our works.</div>
+                    <button className='img-360-button' onClick={open360N4}>Virtual Tour</button>
+                </div>
+            </div>
+            <div className="img-360 img-360-8 img-small-style" onMouseDown={handleMouseDown} onClick={open360N4}></div>
+            <div className="img-360 img-360-9" onMouseDown={handleMouseDown}>
+                <div className="content">
+                    <div className="name">Project Name</div>
+                    <div className="des">Step into a world of wonder with our captivating virtual tour! Explore every corner and detail for our works.</div>
+                    <button className='img-360-button' onClick={open360N5}>Virtual Tour</button>
+                </div>
+            </div>
+            <div className="img-360 img-360-10 img-small-style" onMouseDown={handleMouseDown} onClick={open360N5}></div>
         </div>
+        {/* <div className='small-img-360-shadow'></div> */}
+          <div className='hor-circle-360' onMouseDown={handleMouseDown}>
+            <div className='hor-circle-360-ball'></div>
+          </div>
+          <div className='ver-circle-360' onMouseDown={handleMouseDown}>
+            <div className='ver-circle-360-ball'></div>
+          </div>
+    </div>
         <div className='hero-about-us-content2'>
           <span className='hero-about-us-content2-text1'>
           We offer custom interior design services tailored to your taste and needs. 
